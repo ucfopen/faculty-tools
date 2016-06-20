cd3c413cbe096cb29f4ea682c2d77d868e39a41c
Matthew Emond <me@ucf.edu>
2016-06-20 14:46:31 -0400

    added ims_lti_py manually.

--- faculty_resources/views.py
+++ faculty_resources/views.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, session, request,\
-	make_response, redirect, url_for, Response, jsonify, session
+from flask import Flask, render_template, session, request, redirect, url_for
 
 #OAuth specific
 from ims_lti_py import ToolProvider
@@ -10,51 +9,52 @@ from functools import wraps
 app = Flask(__name__)
 from config import *
 
+
 # ============================================
 # Utility Functions
 # ============================================
-
 def check_valid_user(f):
-   @wraps(f)
-   def decorated_function(*args, **kwargs):
-       """
-       Decorator to check if the user is allowed access to the app.
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        """
+        Decorator to check if the user is allowed access to the app.
 
-       If user is allowed, return the decorated function.
-       Otherwise, return an error page with corresponding message.
-       """
-       canvas_user_id = session.get('canvas_user_id')
+        If user is allowed, return the decorated function.
+        Otherwise, return an error page with corresponding message.
+        """
+        canvas_user_id = session.get('canvas_user_id')
 
-       if not session.get('lti_logged_in') or not canvas_user_id:
-           return render_template('index.html', msg="Login error")
+        if not session.get('lti_logged_in') or not canvas_user_id:
+            return render_template('index.html', msg="Login error")
 
-       if not 'course_id' in kwargs.keys():
-           return render_template('index.html', msg="Course ID error")
+        if not 'course_id' in kwargs.keys():
+            return render_template('index.html', msg="Course ID error")
 
-       course_id = int(kwargs.get('course_id'))
-       enrollments_url = "%scourses/%s/enrollments" % (API_URL, course_id)
-       payload = {
-           'user_id': canvas_user_id,
-           'type': ['TeacherEnrollment', 'TaEnrollment', 'DesignerEnrollment', 'StudentEnrollment', 'ObserverEnrollment']
-       }
+        course_id = int(kwargs.get('course_id'))
+        enrollments_url = "%scourses/%s/enrollments" % (API_URL, course_id)
+        payload = {
+            'user_id': canvas_user_id,
+            'type': [
+                'TeacherEnrollment', 'TaEnrollment', 'DesignerEnrollment',
+                'StudentEnrollment', 'ObserverEnrollment'
+            ]
+        }
 
-       user_enrollments_response = requests.get(
-           enrollments_url,
-           data=json.dumps(payload),
-           headers=json_headers
-       )
-       user_enrollments = user_enrollments_response.json()
-       if not user_enrollments or 'errors' in user_enrollments:
+        user_enrollments_response = requests.get(
+            enrollments_url,
+            data=json.dumps(payload),
+            headers=json_headers
+        )
+        user_enrollments = user_enrollments_response.json()
+        if not user_enrollments or 'errors' in user_enrollments:
             return render_template('index.html', msg="Error with enrollments request")
+        return f(*args, **kwargs)
+    return decorated_function
 
-
-       return f(*args, **kwargs)
-   return decorated_function
 
 # ============================================
 # Web Views / Routes
 # ============================================
-
 @app.route("/<int:course_id>")
 @check_valid_user
 def index(course_id=None):
@@ -71,11 +71,17 @@ def index(course_id=None):
         try:
             current_student = [s for s in students if s['id'] == int(session['canvas_user_id'])][0]
         except:
-            current_student ={id: 0, name:""}
+            current_student = {id: 0, name: ""}
     else:
         students = []
         current_student = []
-    return render_template("index.html", course=course_id, students=students, google=GOOGLE, current_student=current_student)
+    return render_template(
+        "index.html",
+        course=course_id,
+        students=students,
+        google=GOOGLE,
+        current_student=current_student
+    )
 
 
 @app.route("/xml/", methods=['POST', 'GET'])
@@ -86,77 +92,81 @@ def xml():
     """
     print request.url_root
     try:
-    	return render_template('test.xml', url=request.url_root)
+        return render_template('test.xml', url=request.url_root)
     except:
-    	return render_template('index.html', msg="No XML file")
+        return render_template('index.html', msg="No XML file")
+
 
 # ============================================
 # LTI Setup & Config
 # ============================================
 @app.route('/launch', methods=['POST'])
 def lti_tool():
-	"""
-	Bootstrapper for lti.
-	"""
-	course_id = request.form.get('custom_canvas_course_id')
-	canvas_user_id = request.form.get('custom_canvas_user_id')
+    """
+    Bootstrapper for lti.
+    """
+    course_id = request.form.get('custom_canvas_course_id')
+    canvas_user_id = request.form.get('custom_canvas_user_id')
 
-	roles = request.form['ext_roles']
-	if not "Administrator" in roles and not "Instructor" in roles:
-		return render_template('error.html',
-			message='Must be an Administrator or Instructor',
-			params=request.form
-		)
+    roles = request.form['ext_roles']
+    if not "Administrator" in roles and not "Instructor" in roles:
+        return render_template(
+            'error.html',
+            message='Must be an Administrator or Instructor',
+            params=request.form
+        )
 
-	session["is_admin"] = "Administrator" in roles
+    session["is_admin"] = "Administrator" in roles
 
-	key = request.form.get('oauth_consumer_key')
-	if key:
-		secret = oauth_creds.get(key)
-		if secret:
-			tool_provider = ToolProvider(key, secret, request.form)
-		else:
-			tool_provider = ToolProvider(None, None, request.form)
-			print tool_provider
-			tool_provider.lti_msg = 'Your consumer didn\'t use a recognized key'
-			tool_provider.lti_errorlog = 'You did it wrong!'
-			return render_template('error.html',
-				message='Consumer key wasn\'t recognized',
-				params=request.form)
-	else:
-		return render_template('error.html', message='No consumer key')
-	print request, "request"
-	print vars(tool_provider)
-	if not tool_provider.is_valid_request(request):
-		return render_template('error.html',
-			message='The OAuth signature was invalid',
-			params=request.form)
+    key = request.form.get('oauth_consumer_key')
+    if key:
+        secret = oauth_creds.get(key)
 
-	if time() - int(tool_provider.oauth_timestamp) > 60*60:
-		return render_template('error.html', message='Your request is too old.')
+        if secret:
+            tool_provider = ToolProvider(key, secret, request.form)
+        else:
+            tool_provider = ToolProvider(None, None, request.form)
+            print tool_provider
+            tool_provider.lti_msg = 'Your consumer didn\'t use a recognized key'
+            tool_provider.lti_errorlog = 'You did it wrong!'
+            return render_template(
+                'error.html',
+                message='Consumer key wasn\'t recognized',
+                params=request.form)
+    else:
+        return render_template('error.html', message='No consumer key')
 
-	# This does truly check anything, it's just here to remind you  that real
-	# tools should be checking the OAuth nonce
-	if was_nonce_used_in_last_x_minutes(tool_provider.oauth_nonce, 60):
-		return render_template('error.html', message='Why are you reusing the nonce?')
+    if not tool_provider.is_valid_request(request):
+        return render_template(
+            'error.html',
+            message='The OAuth signature was invalid',
+            params=request.form)
 
-	session['canvas_user_id'] = canvas_user_id
-	session['lti_logged_in'] = True
+    if time() - int(tool_provider.oauth_timestamp) > 60*60:
+        return render_template('error.html', message='Your request is too old.')
 
-	session['launch_params'] = tool_provider.to_params()
-	username = tool_provider.username('Dude')
+    # This does truly check anything, it's just here to remind you  that real
+    # tools should be checking the OAuth nonce
+    if was_nonce_used_in_last_x_minutes(tool_provider.oauth_nonce, 60):
+        return render_template('error.html', message='Why are you reusing the nonce?')
 
-	if tool_provider.is_outcome_service():
-		return render_template('assessment.html', username=username)
-	else:
-		return redirect(url_for('quiz', course_id=course_id))
+    session['canvas_user_id'] = canvas_user_id
+    session['lti_logged_in'] = True
+
+    session['launch_params'] = tool_provider.to_params()
+    username = tool_provider.username('Dude')
+
+    if tool_provider.is_outcome_service():
+        return render_template('assessment.html', username=username)
+    else:
+        return redirect(url_for('quiz', course_id=course_id))
 
 
 def was_nonce_used_in_last_x_minutes(nonce, minutes):
-	return False
+    return False
 
 
 if __name__ == "__main__":
-	app.debug = True
-	app.secret_key = secret_key
-	app.run(host="localhost", port=8080)
+    app.debug = True
+    app.secret_key = secret_key
+    app.run(host="127.0.0.1", port=8080)
