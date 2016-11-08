1dcbeaef7f69cf858ab5f120aafe6776557c4659
Elizabeth Williams <elizabeth.williams@ucf.edu>
2016-11-08 16:46:10 -0500

    Reworked sessions, smashed clear bug

--- faculty_resources/views.py
+++ faculty_resources/views.py
@@ -20,7 +20,6 @@ canvas = Canvas(config.API_URL, config.API_KEY)
 # ============================================
 
 
-
 def check_valid_user(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -30,41 +29,56 @@ def check_valid_user(f):
         If user is allowed, return the decorated function.
         Otherwise, return an error page with corresponding message.
         """
-        canvas_user_id = request.form.get('custom_canvas_user_id')
-        if not request.form.get('custom_canvas_user_id') or not canvas_user_id:
+
+        if request.form:
+            # kill old session, rebuild
+            session.clear()
+            session['course_id'] = request.form.get('custom_canvas_course_id')
+            session['canvas_user_id'] = request.form.get('custom_canvas_course_id')
+            roles = request.form['roles']
+            if "Administrator" in roles:
+                session['admin'] = True
+                session['instructor'] = True
+            if "Instructor" in roles:
+                session['instructor'] = True
+
+        # no session and no request
+        if not session:
+            if not request.form:
+                return render_template(
+                    'error.html',
+                    msg='Not allowed!'
+                )
+
+        # no canvas_user_id
+        if not request.form.get('custom_canvas_user_id') and 'canvas_user_id' not in session:
             return render_template(
                 'error.html',
                 msg='Not allowed!'
             )
 
-        course_id = int(request.form.get('custom_canvas_course_id'))
-        if not request.form.get('custom_canvas_course_id') or not course_id:
+        # no course_id
+        if not request.form.get('custom_canvas_course_id') and 'course_id' not in session:
             return render_template(
                 'error.html',
                 msg='No course_id provided.'
             )
 
-        # make sure that they are enrolled in this course
-        course = canvas.get_course(course_id)
-        user_enrollments = course.get_enrollments()
-        enrolled = False
-
-        for enrollment in user_enrollments:
-            if enrollment.user_id == int(canvas_user_id):
-                enrolled = True
-
-        roles = request.form['roles']
-        if "Administrator" in roles:
-            session['admin'] = True
-            session['instructor'] = True
-        if "Instructor" in roles:
-            session['instructor'] = True
-
+        # not permitted
         if 'instructor' not in session:
             return render_template(
                 'error.html',
                 msg='You are not enrolled in this course as a Teacher, TA, or Designer.'
             )
+
+        # make sure that they are enrolled in this course
+        user = canvas.get_user(session['canvas_user_id'])
+        user_enrollments = user.get_enrollments()
+        enrolled = False
+
+        for enrollment in user_enrollments:
+            if enrollment.course_id == int(session['course_id']):
+                enrolled = True
 
         if enrolled is False and 'admin' not in session:
             return render_template(
@@ -80,7 +94,7 @@ def check_valid_user(f):
 # Web Views / Routes
 # ============================================
 @app.route("/")
-# @check_valid_user
+@check_valid_user
 def index():
     """
     Main entry point to web application, call all the things and send the data to the template
@@ -88,7 +102,7 @@ def index():
 
     # Get data from the higher level account
     # account = canvas.get_account(config.UCF_ID)
-    # for dev
+    # 1 for dev
     account = canvas.get_account(1)
     global_ltis = account.get_external_tools()
     course = canvas.get_course(session['course_id'])
@@ -103,8 +117,6 @@ def index():
 
     # load our white list
     json_data = json.loads(open('whitelist.json').read())
-
-    # user = session.get('canvas_user_id')
 
     for lti in lti_requests:
         try:
@@ -124,6 +136,7 @@ def index():
 
         except CanvasException:
             # this lti threw an exception when talking to Canvas
+            # log here
             pass
 
     return render_template(
@@ -160,9 +173,6 @@ def mockup():
 @app.route('/oauthlogin', methods=['POST', 'GET'])
 # @check_valid_user
 def oauth_login():
-    # print "oauthlogin session", session, "oauth login session"
-    # print "oauthlogin request", request.args, "ouathlogin request"
-    # print session.get('canvas_user_id')
 
     code = request.args.get('code')
     payload = {
@@ -176,7 +186,6 @@ def oauth_login():
     if 'access_token' in r.json():
         config.API_KEY = r.json()['access_token']
         # go to index
-        # return render_template("mockup1.html")
         return redirect(url_for('index'))
     else:
         # authentication error
@@ -187,9 +196,6 @@ def oauth_login():
 @app.route('/auth', methods=['POST', 'GET'])
 @check_valid_user
 def auth():
-    session.clear()
-    session['course_id'] = request.form.get('custom_canvas_course_id')
-    session['canvas_user_id'] = request.form.get('custom_canvas_user_id')
 
     # if they aren't in our DB/their token is expired or invalid
     return redirect(config.BASE_URL+'login/oauth2/auth?client_id='+config.oauth2_id +
@@ -200,9 +206,9 @@ def auth():
 # LTI Setup & Config
 # ============================================
 
-@app.route('/launch', methods=['POST'])
-def lti_tool():
-    return "hi"
+# @app.route('/launch', methods=['POST'])
+# def lti_tool():
+#     return "hi"
 
 if __name__ == "__main__":
     app.debug = True
