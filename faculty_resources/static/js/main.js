bbd773900642623c274652d81cddd914033757f3
Elizabeth Williams <elizabeth.williams@ucf.edu>
2016-11-18 17:46:20 -0500

    Added js for the filter, updated config template, added readme

---
+++ faculty_resources/static/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    $(".intro-buttons li a").click(function(e) {
+        $("div.card").hide();
+        e.preventDefault();
+        var filter = $(this).attr("data-filter");
+        $.each($("div.card"), function() {
+            if ($(this).attr("data-filter") !== filter) {
+                    if(!$("div.card").is(":visible")) {
+                        // empty set of cards, show empty msg
+                        if ($("#no_ltis").length == 0) {
+                            $("#all_tools").parent().append("<p class='text-center' id='no_ltis'>No LTIs available in this category</p>");
+                        } else {
+                            $("#no_ltis").fadeIn();
+                        }
+                    }     
+            } else if ($(this).attr("data-filter") == filter) {
+                $("div.card").fadeOut();
+                var card = $(this);
+                if ($("#no_ltis").length > 0) {
+                    $("#no_ltis").hide();
+                    card.fadeIn();
+                } else {
+                    $(this).fadeIn();
+                }
+            }
+        });
+        $("#all_tools").text($(this).text());
+    });
+
+});
