030700a507ee765e8e22772b3d013676997379c8
Elizabeth Williams <elizabeth.williams@ucf.edu>
2016-11-21 15:35:32 -0500

    Simplified the JS and it works better

--- faculty_resources/static/js/main.js
+++ faculty_resources/static/js/main.js
@@ -5,27 +5,20 @@ $(document).ready(function() {
         e.preventDefault();
         var filter = $(this).attr("data-filter");
         $.each($("div.card"), function() {
-            if ($(this).attr("data-filter").indexOf(filter) == -1) {
-                    if(!$("div.card").is(":visible")) {
-                        // empty set of cards, show empty msg
-                        if ($("#no_ltis").length == 0) {
-                            $("#all_tools").parent().append("<p class='text-center' id='no_ltis'>No LTIs available in this category</p>");
-                        } else {
-                            $("#no_ltis").fadeIn();
-                        }
-                    }     
-            } else if ($(this).attr("data-filter").indexOf(filter) > -1) {
-                $("div.card").fadeOut();
-                var card = $(this);
-                if ($("#no_ltis").length > 0) {
-                    $("#no_ltis").hide();
-                    card.fadeIn();
-                } else {
-                    $(this).fadeIn();
-                }
+            if ($(this).attr("data-filter").indexOf(filter) > -1) {
+                $(this).fadeIn();
+                $("#no_ltis").hide();
             }
         });
         $("#all_tools").text($(this).text());
+        if(!$("div.card").is(":visible")) {
+            // empty set of cards, show empty msg
+            if ($("#no_ltis").length == 0) {
+                $("#all_tools").parent().append("<p class='text-center' id='no_ltis'>No LTIs available in this category</p>");
+            } else {
+                $("#no_ltis").fadeIn();
+            }
+        }
     });
 
 });
