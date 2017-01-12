68cfaf6d7d3879666f189db5b6af7f1dba419157
Shea Silverman <ssilverm@ucf.edu>
2017-01-12 15:30:30 -0500

    filter_by

--- faculty_resources/views.py
+++ faculty_resources/views.py
@@ -389,7 +389,7 @@ def oauth_login():
         try:
 
             # add to db
-            user = Users.query.get(user_id=int(session['canvas_user_id']))
+            user = Users.query.filter_by(user_id=int(session['canvas_user_id']))
             if user:
                 print "trying to update old user"
                 user.refresh_token = session['refresh_token']
@@ -439,7 +439,7 @@ def auth():
     # if they aren't in our DB/their token is expired or invalid
     try:
         # Try to grab the user
-        user = Users.query.get(int(session['canvas_user_id']))
+        user = Users.query.filter_by(user_id=int(session['canvas_user_id']))
 
         # Found a user
         if user:
