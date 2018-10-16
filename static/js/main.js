86f5ca118df79bb017be2f4d81404c4898f3ce81
Shea Silverman <shea.silverman@ucf.edu>
2018-10-16 14:57:31 -0400

    resizeFrame

--- static/js/main.js
+++ static/js/main.js
@@ -33,4 +33,24 @@ $(document).ready(function() {
 
         });
     });
+
+    // resize containing iframe height
+    function resizeFrame(){
+        var default_height = $('body').height() + 50;
+        default_height = default_height > 500 ? default_height : 500;
+
+        // IE 8 & 9 only support string data, so send objects as string
+        parent.postMessage(JSON.stringify({
+          subject: "lti.frameResize",
+          height: default_height
+        }), "*");
+    }
+
+    // update iframe height on resize
+    $(document).on('resize', function(){
+        resizeFrame();
+    });
+
+    resizeFrame();
+
 });
