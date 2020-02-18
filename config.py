2f1586b2398c6e7968bfa1458901b8bafc0ba64c
Matthew Emond <me@ucf.edu>
2020-02-18 18:49:06 -0500

    SameSite fix. Update packages. (#15)
    Fixes #14

--- config.py
+++ config.py
@@ -2,32 +2,45 @@ import settings
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = settings.select_db("Config")
+    # make the warning shut up until Flask-SQLAlchemy v3 comes out
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = settings.select_db("Config")
+
     PYLTI_CONFIG = settings.PYLTI_CONFIG
+
     SESSION_COOKIE_NAME = "ft_session"
+
+    # Chrome 80 SameSite=None; Secure fix
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_SAMESITE = "None"
 
 
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = settings.select_db("BaseConfig")
+
+    # make the warning shut up until Flask-SQLAlchemy v3 comes out
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = settings.select_db("BaseConfig")
+
     PYLTI_CONFIG = settings.PYLTI_CONFIG
+
+    SESSION_COOKIE_NAME = "ft_session"
+
+    # Chrome 80 SameSite=None; Secure fix
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_SAMESITE = "None"
 
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
     TESTING = True
+
     SQLALCHEMY_DATABASE_URI = settings.select_db("DevelopmentConfig")
-    # make the warning shut up until Flask-SQLAlchemy v3 comes out
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    PYLTI_CONFIG = settings.PYLTI_CONFIG
 
 
 class TestingConfig(BaseConfig):
     DEBUG = False
     TESTING = True
+
     SQLALCHEMY_DATABASE_URI = settings.select_db("TestingConfig")
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    PYLTI_CONFIG = settings.PYLTI_CONFIG
